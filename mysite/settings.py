75f329d6a77cbed378783b87969fc511b6a709d4
Leela Muppala <leela.sudha01@gmail.com>
2020-08-01 02:11:06 +0100

    Modify 'DEBUG=True to DEBUG=Flase'

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '))321ctyaawyaf5b$^mk3fdkwx**aebbe1p+l_+i@l1v6)h2x#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
